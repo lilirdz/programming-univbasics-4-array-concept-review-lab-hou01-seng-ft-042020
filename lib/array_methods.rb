15260a7e075bf1f324c23d0482a09e052adb0c4d
Lili Rodriguez <lilimrod@gmail.com>
2020-03-11 04:45:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,11 +20,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  x = array[0]
+  min = array[0]
   array.length.times do |index|
-    if array[index] < x
-      x = array[index]
+    if array[index] < min
+      min = array[index]
     end 
   end
-  x
+  min
 end
